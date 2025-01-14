d96fbd807e75ec91aac13edef2a0bb55143c7384
Rune Sjöstedt <RuneAlfa@hotmail.com>
2025-01-14 01:06:07 +0100

    started with user menu

---
+++ Console.MainApp/Services/UserMenu.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace MainApp.Services;
+
+public class UserMenu
+{
+    static void Main(string[] args)
+    {
+        bool exit = false;  
+
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("=====Contactlist Menu=====");
+            Console.WriteLine("1. Create a Contact");
+            Console.WriteLine("2. View Contacts");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine("===========================");
+            Console.Write("Choose an Option: ");
+
+            String userInput = Console.ReadLine();
+            
+            switch (userInput)
+            {
+                case "1":
+
+                    break;
+
+                case "2":
+
+                    break;
+
+                case "3":
+                    exit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid selection. Try again");
+                    break;
+            }
+        }
+    }
+}
